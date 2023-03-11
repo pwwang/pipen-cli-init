eeead46bdec9f7ec825493b9f3866e4232df096c
pwwang <pwwang@pwwang.com>
2023-03-10 19:03:05 -0700

    🔖 0.4.0

--- pipen_cli_init/__init__.py
+++ pipen_cli_init/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from pipen.cli._hooks import CLIPlugin
 from copier import run_auto
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 if TYPE_CHECKING:
     from argx import ArgumentParser, Namespace
