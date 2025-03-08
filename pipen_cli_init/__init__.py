4d930fabf3d513f57f0ada3879822bf8575f2867
pwwang <pwwang@pwwang.com>
2025-03-07 21:55:57 -0700

    chore: bump version to 0.13.1

--- pipen_cli_init/__init__.py
+++ pipen_cli_init/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from pipen.cli._hooks import CLIPlugin
 from copier import run_copy
 
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 
 if TYPE_CHECKING:
     from argx import ArgumentParser, Namespace
